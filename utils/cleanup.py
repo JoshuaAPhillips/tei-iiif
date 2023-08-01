5c18c29180afb04ed11d7d2510f26abb25a1ff1c
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-01 19:45:16 +0100

    cleanup.py: functions to clean up temporary xml files and temp directory

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -1,1 +1,30 @@
+from utils.settings import settings
+import logging
 import os
+import shutil
+
+temp_dir = settings["temp_dir"]
+
+def xmlCleaner() -> None:
+  """
+  Cleans up temporary XML files
+  """
+  try:
+    for file in temp_dir:
+        if file.endswith(".xml"):
+            os.remove(file)
+        else:
+            pass
+  except FileNotFoundError as e:
+      logging.error(e)
+
+def dirCleaner() -> None:
+  """
+  Removes up temp directory
+  """
+  try:
+    shutil.rmtree(temp_dir)
+  except OSError as e:
+    logging.error(f"Error: {e.filename} - {e.strerror}.")
+
+  logging.info('Cleaning up temporary files...')
