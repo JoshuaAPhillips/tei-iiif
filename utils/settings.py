14d6e0e1b2bb08ee3bf0b2df9f32fb75dd0e2f14
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-01 09:15:44 +0100

    settings.py: set up basic error handling

--- utils/settings.py
+++ utils/settings.py
@@ -1,15 +1,17 @@
 import yaml
-import os
+import logging
 
 def settings() -> dict:
 
   """
   Loads settings from ../settings.yaml and returns as a dictionary.
   """
-
-  with open("settings.yaml", 'r') as f:
-      settings = yaml.load(f, Loader=yaml.FullLoader)
-      
-  print(settings)
+  try:
+    with open("settings.yaml", 'r') as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+        
+    print(settings)
+  except FileNotFoundError as e:
+     logging.ERROR(e)
 
   return settings
