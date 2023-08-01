24f23d4a97bed260b552690973b5a7936c6d7562
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-01 19:48:03 +0100

    main.py: importing utils.cleanup

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from utils.settings import settings
+from utils.cleanup import xmlCleaner, dirCleaner
+from getfile import fileName, getFile
 
 def main():
+    """
+    Does the thing
+    """
     settings()
+    filename = fileName()
+    r = getFile(filename)
 
 if __name__ == '__main__':
     main()
