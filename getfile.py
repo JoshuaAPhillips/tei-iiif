58556a18e5ab594133e68465b3000f800062fb3c
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-01 10:38:09 +0100

    getfile.py: downloads successfully

--- getfile.py
+++ getfile.py
@@ -1,2 +1,29 @@
+import sys
+import logging
 import requests
 from utils.settings import settings
+
+settings = settings()
+
+def fileName() -> str:
+    base_url = settings["base_path"]
+    filename = sys.argv[-1]
+
+    if settings["base_path"] is not None:
+      file = base_url + filename
+      print(file)
+      return file
+    else:
+       logging.error('Check "base_path" and "file_name" are set')
+
+def getFile(file) -> object:
+   if file.startswith("http"):
+      try:
+         r = requests.get(file)
+         print(r)
+      except FileNotFoundError as e:
+         logging.error("File not found:" + e)
+
+
+file = fileName()
+getFile(file)
