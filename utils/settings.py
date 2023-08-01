4b86d8157d05abe22d00496b63644e2ba8461119
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-01 08:56:11 +0100

    initial commit

---
+++ utils/settings.py
@@ -0,0 +1,15 @@
+import yaml
+import os
+
+def settings() -> dict:
+
+  """
+  Loads settings from ../settings.yaml and returns as a dictionary.
+  """
+
+  with open("../settings.yaml", 'r') as f:
+      settings = yaml.load(f, Loader=yaml.FullLoader)
+      
+  print(settings)
+
+  return settings
