7ee28e2ed307001cfac48f15abe279f7f11b193f
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-03 20:39:07 +0100

    __main__.py: Now does the thing all the way

--- __main__.py
+++ __main__.py
@@ -1,8 +1,10 @@
 from utils.settings import settings
+from utils.cleanup import dirCleaner
 from getfile import fileName, getFile
 from xmlparser import getRoot, divList, metadata
 from tempfilegen import tempFileGen
 from divjson import divJson
+from manifestgen import manifestGen, jsonSave
 
 settings = settings()
 temp_dir = settings["temp_dir"]
@@ -15,10 +17,13 @@ def main():
     r = getFile(filename)
     root = getRoot(r)
     div_list = divList(root)
-    metadata(root)
+    metadata_dict = metadata(root)
     tempFileGen(div_list)
     divJson(temp_dir)
-    #don't actually need to clean up xml, just implement a .endswith(".json") in the next step...
+    manifest_list = manifestGen(metadata_dict["idno"], settings["temp_dir"], settings["manifest_store"])
+    jsonSave(manifest_list, metadata_dict["idno"])
+    dirCleaner(temp_dir)
+
 
 if __name__ == '__main__':
     main()
