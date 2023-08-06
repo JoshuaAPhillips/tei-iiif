2767c454d27e613f95d6096bf8be37055b920f5d
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-06 12:08:47 +0100

    moved code for module into its own folder: enables running as module

---
+++ tei-iiif/__main__.py
@@ -0,0 +1,41 @@
+import logging
+from utils.settings import settings
+from utils.cleanup import dirCleaner
+from getfile import fileName, getFile
+from xmlparser import getRoot, divList, metadata
+from tempfilegen import tempFileGen
+from divjson import divJson
+from manifestgen import manifestGen, jsonSave
+
+settings = settings()
+temp_dir = settings["temp_dir"]
+logging_level = settings["logging"]["level"]
+log_file = settings["logging"]["file"]
+
+logging.basicConfig(
+    level=logging_level, 
+    format='%(asctime)s - %(levelname)s -%(name)s - %(message)s', 
+    filename=log_file, 
+    filemode="w", 
+    force=True
+    )
+
+def main():
+    """
+    Does the thing
+    """
+    filename = fileName()
+    r = getFile(filename)
+    root = getRoot(r)
+    div_list = divList(root)
+    metadata_dict = metadata(root)
+    tempFileGen(div_list)
+    divJson(temp_dir)
+    manifest_list = manifestGen(metadata_dict["idno"], settings["temp_dir"], settings["manifest_store"])
+    jsonSave(manifest_list, metadata_dict["idno"])
+    dirCleaner(temp_dir)
+    logging.info("Done!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ tei-iiif/getfile.py
@@ -0,0 +1,52 @@
+import sys
+import logging
+import requests
+from utils.settings import settings
+
+settings = settings()
+
+def fileName() -> str:
+    """
+    Returns `filename` from command line argument and path specified in settings.yaml
+    - (TODO: work on way to accept list of files and iterate through them)
+    """
+    base_url = settings["base_path"]
+    filename = sys.argv[-1]
+
+    if settings["base_path"] is not None:
+      file = base_url + filename
+      return file
+    else:
+       logging.error('Check "base_path" and "file_name" are set', exc_info=True)
+
+def getFile(file) -> object:
+   """
+   Returns `file` from local or remote store for parsing in `xmlparser.py`
+   - Uses requests library to fetch remote files
+      - Uses basic auth to fetch remote files: if auth required, set username and password in settings.yaml
+      - If auth not required, leave username and password blank
+      - If more advanced authentication systems such as OAuth are required, see https://requests.readthedocs.io/en/latest/user/authentication/ for implementation
+   - Uses open() to fetch local files
+   """
+
+   if file.startswith("http"):
+      try:
+         r = requests.get(file, auth=(settings["request_params"]["username"], settings["request_params"]["password"]))
+         if r.status_code == 200:
+            logging.debug(f"Successfully fetched {file} from remote store")
+            return r
+         else:
+            logging.error(r.status_code)
+      except Exception as e:
+         logging.error(e, exc_info=True)
+         logging.error(f"Failed to fetch {file} from remote store. Quitting...")
+
+   else:
+      try:
+         r_wrapper = open(file, "r")
+         r = r_wrapper.read()
+         logging.debug(f"Successfully fetched {file} from local store")
+         return r
+      except FileNotFoundError as e:
+         logging.error(e, exc_info=True)
+         logging.error(f"Failed to fetch {file} from local store. Quitting...")
