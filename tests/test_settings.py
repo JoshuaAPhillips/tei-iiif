450894101766733577bfba1efa24a48eee99967f
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-09 14:56:40 +0100

    test_settings.py: initial commit

---
+++ tests/test_settings.py
@@ -0,0 +1,14 @@
+import pytest
+from tei_iiif.utils.settings import settings
+
+@pytest.fixture(scope="module")
+def get_settings():
+    return settings
+
+def test_settings_pass(get_settings):
+    settings_dict = get_settings()
+    assert settings_dict["temp_dir"] == "./temp/"
+
+def test_settings_fail(get_settings):
+    settings_dict = get_settings()
+    assert settings_dict["temp_dir"] is None
