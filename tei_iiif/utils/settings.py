86a2ff94b11e15f541247f28efab9cded8dcc231
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-13 14:31:55 +0100

    settings.py: refactored to use importlib.resources - now works when installed as package

--- tei_iiif/utils/settings.py
+++ tei_iiif/utils/settings.py
@@ -1,5 +1,6 @@
 import yaml
 import logging
+import importlib.resources
 
 
 def settings() -> dict:
@@ -8,10 +9,10 @@ def settings() -> dict:
   Loads settings from `../settings.yaml` and returns as a dictionary so that other functions can access.
   """
   try:
-    with open("tei_iiif/settings.yaml", 'r') as f:
-        settings = yaml.load(f, Loader=yaml.FullLoader)
-        logging.info("Settings loaded.")
-        return settings
+    settings_file = importlib.resources.read_text("tei_iiif", "settings.yaml")
+    settings = yaml.safe_load(settings_file)
+    logging.info("Settings loaded.")
+    return settings
 
   except FileNotFoundError as e:
      logging.error(e)
