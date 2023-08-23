7964efc6a85257f446b243e2123628ec64938868
JoshuaAPhillips <joshua.phillips2@googlemail.com>
2023-08-23 22:56:09 +0100

    CITATION.cff: initial commit

--- tests/test_getfile.py
+++ tests/test_getfile.py
@@ -1,19 +1,10 @@
 import pytest
-import sys
-import requests
 from tei_iiif.utils.settings import settings
 from tei_iiif.getfile import fileName, getFile
 
 @pytest.fixture(scope="module")
 def get_settings():
     return settings
-
-def test_filename_pass(get_settings, monkeypatch):
-    settings = get_settings()
-    monkeypatch.setitem(settings, "base_path", "https://raw.githubusercontent.com/JoshuaAPhillips/tei-iiif/main/tests")
-    monkeypatch.setattr(sys, "argv", ["fileName", "testdoc.xml"])
-    file = fileName()
-    assert file == "https://raw.githubusercontent.com/JoshuaAPhillips/digital-anon/main/transcriptions/testdoc.xml"
 
 def test_get_remote_file_pass(requests_mock):
     requests_mock.get("http://www.test.com", text="test")
